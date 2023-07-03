a84685b8b8e4bffffa12661d3373126b5c3c6f69
Clément Prod'homme <clement.prod-homme@captive.fr>
2023-07-03 10:34:01 +0200

    ⚡️ Optimise la méthode pagination
    Lorsque celle ci est appelé plusieurs fois

--- app/controllers/concerns/api/pagination_concern.rb
+++ app/controllers/concerns/api/pagination_concern.rb
@@ -15,10 +15,12 @@ module Api
       DEFAULT_NB_ITEMS_PAR_PAGE = 10
 
       def pagination
-        par_page = (params[:per_page] || DEFAULT_NB_ITEMS_PAR_PAGE).to_i
-        par_page = MAX_ITEMS_PAR_PAGE if par_page > MAX_ITEMS_PAR_PAGE
-        page = (params[:page] || 1).to_i
-        @pagination ||= { page: page, per_page: par_page }
+        @pagination ||= begin
+          par_page = (params[:per_page] || DEFAULT_NB_ITEMS_PAR_PAGE).to_i
+          par_page = MAX_ITEMS_PAR_PAGE if par_page > MAX_ITEMS_PAR_PAGE
+          page = (params[:page] || 1).to_i
+          { page: page, per_page: par_page }
+        end
       end
     end
   end
